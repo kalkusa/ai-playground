11fb2928857ae405e9d328b55d69c5a3ec5d426f
Arkadiusz Kałkus <kalkus@10g.pl>
2024-04-18 20:27:38 +0200

    Stable diffusion experiments

--- 002 - stable diffusion/main.py
+++ 002 - stable diffusion/main.py
@@ -7,10 +7,14 @@ print(torch.cuda.is_available())
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe.to("cuda")
 
-prompt = "A monkey in front of computer with software development editor on the screen. 16k, ultra photo realism, golden hour."
+# prompt = "portrait of a pretty blonde woman, a flower crown, earthy makeup, flowing maxi dress with colorful patterns and fringe, a sunset or nature scene, green and gold color scheme"
+# prompt = "close up photo of a rabbit, forest in spring, haze, halation, bloom, dramatic atmosphere, centred, rule of thirds, 200mm 1.4f macro shot"
+# prompt = "a cat under the snow with blue eyes, covered by snow, cinematic style, medium shot, professional photo, animal"
+# prompt = "freshly made jasmine tea in exquisite chinese teacup set on the table, angled shot, midday warm, Nikon D850 105mm, close-up"
+prompt = "3 small Netherlands type sunflowers in a small white matt tall vase on a white table, midday warm, Nikon D850 105mm, medium shot, professional photo,"
 result = pipe(prompt=prompt)
 image = result.images[0]
-image_path = "monke.png"
+image_path = "sunflowers.png"
 image.save(image_path)
 
 print(f"Image saved at {image_path}")
