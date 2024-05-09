63e462c95f504e5ec9c5db9f8f01f1310e384b71
Arkadiusz Kałkus <kalkus@10g.pl>
2024-05-09 22:48:01 +0200

    Add models and set up model loading.

--- 004  - face recognition/src/Components/FaceRecognition.tsx
+++ 004  - face recognition/src/Components/FaceRecognition.tsx
@@ -1,15 +1,37 @@
 import { useEffect, useRef } from "react";
 import styles from "./FaceRecognition.module.css";
+import * as faceapi from "face-api.js";
 
 const FaceRecognition = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const loadModels = async () => {
+      try {
+        const MODEL_URL = process.env.PUBLIC_URL + "/models";
+
+        await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+        await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+        await faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL);
+        await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+        await faceapi.nets.mtcnn.loadFromUri(MODEL_URL);
+        await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+        await faceapi.nets.faceLandmark68TinyNet.loadFromUri(MODEL_URL);
+        await faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL);
+
+        console.log("Models loaded");
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    loadModels();
+  }, []);
 
   useEffect(() => {
     const startVideoStreaming = async () => {
       try {
         const constraints = { audio: false, video: true };
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        // console.log("Got MediaStream:", stream);
 
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
@@ -30,8 +52,14 @@ const FaceRecognition = () => {
   }, []);
 
   return (
-    <section id="face-recognition" >
-      <video id="face-recognition-video" className={styles.video} ref={videoRef} autoPlay muted />
+    <section id="face-recognition">
+      <video
+        id="face-recognition-video"
+        className={styles.video}
+        ref={videoRef}
+        autoPlay
+        muted
+      />
     </section>
   );
 };
